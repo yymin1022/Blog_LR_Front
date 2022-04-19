e5d42108a49a478e597e494997448edcf3e962e0
yymin1022 <yymin1022@gmail.com>
2022-04-19 13:21:02 +0900

    Add PostList Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
+import PostList from "./Pages/PostList";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
 		<Routes>
 			<Route element={<></>}>
 				<Route path="/" element={<Home/>} 	/>
-				<Route path="/postlist/:postType" element={<></>} />
+				<Route path="/postlist/:postType" element={<PostList/>} />
 				<Route path="/postview/:posttype/:postID" element={<></>} />
 			</Route>
 		</Routes>
