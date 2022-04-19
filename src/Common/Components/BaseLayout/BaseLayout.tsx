d932a661f10be03bfd0dd92bd70bbffa50520001
yymin1022 <yymin1022@gmail.com>
2022-04-19 13:43:54 +0900

    Update Package Info

--- src/Common/Components/BaseLayout/BaseLayout.tsx
+++ src/Common/Components/BaseLayout/BaseLayout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const BaseLayout = () => {
     return (
         <div>
-            Base Layout
+            <Outlet />
         </div>
     );
 };
