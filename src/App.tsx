4746a95cb914ee4d446110d7f8d0303668dc6129
yymin1022 <yymin1022@gmail.com>
2022-04-19 13:21:38 +0900

    Add PostView Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import PostList from "./Pages/PostList";
+import PostView from "./Pages/PostView";
 import "./App.css";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 			<Route element={<></>}>
 				<Route path="/" element={<Home/>} 	/>
 				<Route path="/postlist/:postType" element={<PostList/>} />
-				<Route path="/postview/:posttype/:postID" element={<></>} />
+				<Route path="/postview/:posttype/:postID" element={<PostView/>} />
 			</Route>
 		</Routes>
 	);
