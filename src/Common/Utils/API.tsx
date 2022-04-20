5525d3e4e27994990acd0588668cd95ce45353ef
yymin1022 <yymin1022@gmail.com>
2022-04-20 14:40:54 +0900

    Update Util/API.tsx : Add Basic Functions

---
+++ src/Common/Utils/API.tsx
@@ -0,0 +1,11 @@
+const getPostData = () => {
+    return {getPostData: true};
+};
+
+const getPostImage = () => {
+    return {getPostImage: true};
+};
+
+const getPostList = () => {
+    return {getPostList: true};
+};
