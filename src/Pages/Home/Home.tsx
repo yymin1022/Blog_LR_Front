ebc4deeee7c14398c0e57b33436fdbae97beea33
yymin1022 <yymin1022@gmail.com>
2022-04-20 09:47:06 +0900

    Update Home.tsx : Add Blog Introduction

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <HomeContainer>
+            <HomeTitleContainer>
+                <HomeTitleText>안녕하세요</HomeTitleText>
+                <HomeTitleText color="blue">대학생 1인 개발자</HomeTitleText>
+                <HomeTitleText>LR입니다</HomeTitleText>
+            </HomeTitleContainer>
+        </HomeContainer>
     );
 };
+
+const HomeContainer = styled.div`
+    margin-left: 100px;
+`;
+
+const HomeTitleContainer = styled.div`
+    margin: 10px;
+`;
+
+const HomeTitleText = styled.p`
+    font-size: 80px;
+    margin: 0px;
+
+    ${(props) =>
+        props.color == "blue" &&
+        css`
+            color: #164EAB;
+            font-weight: bold;
+        `
+    }
+`;
 
 export default Home;
