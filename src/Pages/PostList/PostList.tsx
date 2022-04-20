680b8a7429f6950d25774320e09f6a7268b3ba06
yymin1022 <yymin1022@gmail.com>
2022-04-20 21:17:09 +0900

    Update PostList.tsx : Add Debug Log

--- src/Pages/PostList/PostList.tsx
+++ src/Pages/PostList/PostList.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import * as API from "../../Common/Utils/API";
 
 const PostList = () => {
+    const postType = useParams();
+    console.log(postType);
+
     return (
         <div>
             PostList
