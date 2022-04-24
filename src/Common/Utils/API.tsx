7132dded88945526e1d61b3b0dc27d017d77c782
yymin1022 <yymin1022@gmail.com>
2022-04-24 19:25:25 +0900

    Update API.tsx : Improved Structure

--- src/Common/Utils/API.tsx
+++ src/Common/Utils/API.tsx
@@ -3,34 +3,43 @@ import axios from "axios";
 const API_URL_BASE : string = process.env.REACT_APP_API_URL as string;
 
 const apiRequest = (apiURL : string, apiReqData : object) => {
-    let apiResult : any = sendRequest(apiURL, apiReqData);
-    console.log(apiResult);
-    
-    let apiResultCode = apiResult["RESULT_CODE"];
-    let apiResultData = apiResult["RESULT_DATA"];
-    let apiResultMsg = apiResult["RESULT_MSG"];
-
-    if(apiResultCode != 200){
+    sendRequest(apiURL, apiReqData).then((apiResult : any) => {
         console.log(apiResult);
-        console.log(apiResultMsg);
-    }
+        
+        let apiResultCode = apiResult["RESULT_CODE"];
+        let apiResultData = apiResult["RESULT_DATA"];
+        let apiResultMsg = apiResult["RESULT_MSG"];
 
-    return(apiResultData);
+        if(apiResultCode != 200){
+            console.log(apiResult);
+            console.log(apiResultMsg);
+        }
+
+        return(apiResultData);
+    });
+    
 };
 
-const sendRequest = (url : string, data : object) => {
-    axios.post(url, data)
-    .then((response) => {
-        console.log(response);
-        return response.data;
-    })
-    .catch((error) => {
-        let resultData = {
-            RESULT_CODE: 100,
-            RESULT_MSG: error as string
-        };
+const sendRequestPromise = new Promise((resolve, reject) => {
 
-        return resultData;
+});
+
+const sendRequest = (url : string, data : object) => {
+    return new Promise((resolve) => {
+        let resultData = {};
+        axios.post(url, data)
+        .then((response) => {
+            console.log(response);
+            resultData = response.data;
+        })
+        .catch((error) => {
+            resultData = {
+                RESULT_CODE: 100,
+                RESULT_MSG: error as string
+            };
+        });
+
+        resolve(resultData);
     });
 }
 
@@ -41,7 +50,7 @@ export const getPostData = (postID : string, postType : string) => {
         postType: postType
     };
 
-    let apiResult : object = apiRequest(apiURL, apiReqData);
+    let apiResult = apiRequest(apiURL, apiReqData);
     return apiResult;
 };
 
@@ -63,6 +72,6 @@ export const getPostList = (postType : string) => {
         postType: postType
     };
 
-    let apiResult : object = apiRequest(apiURL, apiReqData);
+    let apiResult = apiRequest(apiURL, apiReqData);
     return apiResult;
 };
