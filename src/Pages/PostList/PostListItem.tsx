6a378b1d3e676c335a38ef94633cf37407ade7a6
yymin1022 <yymin1022@gmail.com>
2022-04-25 19:02:29 +0900

    Update PostListItem.tsx : Add Post Data Props for PostListItem

--- src/Pages/PostList/PostListItem.tsx
+++ src/Pages/PostList/PostListItem.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const PostListItem = (props : any) => {
+    const [postDate, setDate] = useState<string>("");
+    const [postIsPinned, setIsPinned] = useState<string>("");
+    const [postTag, setTag] = useState<string>("");
+    const [postTitle, setTitle] = useState<string>("");
+    const [postType, setType] = useState<string>("");
+    const [postURL, setURL] = useState<string>("");
+
+    useEffect(() => {
+        setDate(props.postDate);
+        setIsPinned(props.postIsPinned);
+        setTag(props.postTag);
+        setTitle(props.posttitle);
+        setType(props.postType);
+        setURL(`/postview/${postType}/${postURL}`);
+    }, []);
+
     return(
-        <p>{props.postTitle}</p>
+        <p>{postTitle}</p>
     )
 }
 
