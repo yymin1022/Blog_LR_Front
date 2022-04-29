0d333500ca236179144f57a944e8299ee74f8c0a
yymin1022 <yymin1022@gmail.com>
2022-04-29 21:08:05 +0900

    Update PostView.tsx : Add Basic Variables

--- src/Pages/PostView/PostView.tsx
+++ src/Pages/PostView/PostView.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PostView = () => {
+    const [postContent, setContent] = useState<string>();
+    const [postDate, setDate] = useState<string>();
+    const [postTag, setTag] = useState<string>();
+    const [postTitle, setTitle] = useState<string>();
+    const [postURL, setURL] = useState<string>();
+
+    useEffect(() => {
+        
+    }, []);
+
     return (
         <div>
             PostView
