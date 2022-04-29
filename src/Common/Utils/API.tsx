c093547f8449b8b9e064f49c8a2a6528760e5db4
yymin1022 <yymin1022@gmail.com>
2022-04-29 21:20:34 +0900

    Update PostView.tsx : Debug API

--- src/Common/Utils/API.tsx
+++ src/Common/Utils/API.tsx
@@ -14,6 +14,8 @@ const apiRequest = (apiURL : string, apiReqData : object) => {
                 console.log(apiResultMsg);
             }
 
+            console.log(apiResultData);
+
             resolve(apiResultData);
         });
     })
