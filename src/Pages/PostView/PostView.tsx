485568ea97667535c2a45e58ec869366802a49dd
yymin1022 <yymin1022@gmail.com>
2022-04-29 21:11:22 +0900

    Update PostView.tsx : Add API Connection Logic

--- src/Pages/PostView/PostView.tsx
+++ src/Pages/PostView/PostView.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import * as API from "../../Common/Utils/API";
 
 const PostView = () => {
+    const {postID} = useParams<{postID : string}>();
+    const {postType} = useParams<{postType : string}>();
+
     const [postContent, setContent] = useState<string>();
     const [postDate, setDate] = useState<string>();
     const [postTag, setTag] = useState<string>();
@@ -8,7 +14,13 @@ const PostView = () => {
     const [postURL, setURL] = useState<string>();
 
     useEffect(() => {
-        
+        API.getPostData(postID as string, postType as string).then((apiResult : any) => {
+            setContent(apiResult["PostContent"]);
+            setDate(apiResult["PostDate"]);
+            setTag(apiResult["PostTag"]);
+            setTitle(apiResult["PostTitle"]);
+            setURL(apiResult["PostURL"]);
+        });
     }, []);
 
     return (
