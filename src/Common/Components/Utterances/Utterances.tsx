b90ef1030d6e7275fcad7b441f2aa2a24353e16a
yymin1022 <yymin1022@gmail.com>
2022-04-30 20:43:32 +0900

    Update Utterances.tsx : Migrated from Legacy

--- src/Common/Components/Utterances/Utterances.tsx
+++ src/Common/Components/Utterances/Utterances.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const Utterances = () => {
+    const utterancesRef = useRef<HTMLDivElement>();
+
+    useEffect(() => {
+        const utterances = document.createElement("script");
+        utterances.setAttribute("src", "https://utteranc.es/client.js");
+        utterances.setAttribute("async", "true");
+        utterances.setAttribute("crossorigin", "anonymus");
+        utterances.setAttribute("repo", "yymin1022/Blog_LR_Comments");
+        utterances.setAttribute("theme", "github-light");
+        utterances.setAttribute("issue-term", "pathname");
+        utterancesRef.current.appendChild(utterances);
+    }, []);
+
     return(
-        <div>
-            Utterances Service
-        </div>
+        <div ref={utterancesRef}></div>
     )
 }
 
