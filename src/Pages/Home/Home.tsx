efac50ed30f47c347772a6610c59ca11e4b031ce
yymin1022 <yymin1022@gmail.com>
2022-05-01 14:47:53 +0900

    Update Home.tsx : Adjust Navigate BTN Destination

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -52,27 +52,30 @@ const HomeNavContainer = styled.div`
 
 const HomeNavItem = (props : any) => {
     let btnString : string = "";
-    let btnGo : string = "postlist";
+    let btnTo : string = "";
     let postType : string = props.postType as string;
 
     switch(postType){
         case "blog":
             btnString = "Blog";
+            btnTo = "/postlist/blog";
             break;
         case "project":
             btnString = "Project";
+            btnTo = "/postlist/project";
             break;
         case "solving":
             btnString = "Problem Solving";
+            btnTo = "/postlist/solving";
             break;
         case "about":
             btnString = "About";
-            btnGo = "postview";
+            btnTo = "/postview/about/LR";
             break;
     }
 
     return (
-        <Link to={`/${btnGo}/${postType}`}>
+        <Link to={btnTo}>
             <p>{btnString}</p>
         </Link>
     )
