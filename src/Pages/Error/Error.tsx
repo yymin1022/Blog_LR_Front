9bfa5a18734ff85a1c5e82e78d26524ee2c0852b
yymin1022 <yymin1022@gmail.com>
2022-05-08 18:40:17 +0900

    Update Error.tsx : Add Basic Structure

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -3,10 +3,20 @@ import styled, { css } from "styled-components";
 
 const Error = () => {
     return (
-        <div>
-            Error Page !
-        </div>
+        <ErrorContainer>
+            
+        </ErrorContainer>
     );
 };
+
+const ErrorContainer = styled.div`
+    width: 100%;
+
+    margin-left: 100px;
+
+    @media screen and (max-width: 1400px){
+        margin: 0px;
+    }
+`;
 
 export default Error;
